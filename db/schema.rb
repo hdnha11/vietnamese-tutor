a031ba312a5bc78162bdb907a53b09c776400e6a
Thanh Dong <thanh.dqblue@gmail.com>
2016-01-14 20:51:01 +0700

    m4

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114132221) do
+ActiveRecord::Schema.define(version: 20160114134359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flash_cards", force: :cascade do |t|
+    t.string   "term"
+    t.string   "definition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
