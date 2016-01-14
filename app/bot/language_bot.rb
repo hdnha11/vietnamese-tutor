8241129f8694673c5272903ff7f0c60d951418ea
Thanh Dong <thanh.dqblue@gmail.com>
2016-01-14 20:18:50 +0700

    m2

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,3 +1,9 @@
 class LanguageBot < SlackRubyBot::Bot
-  # leave it empty for now
+	command 'hello' do |client, data, match|
+		I18n.locale = :vi
+		# client.say(text: "#{I18n.t "hello"} <@#{data.user}>!", channel: data.channel)
+		text = I18n.t "hello_2", user: data.user
+		client.say(text: text, channel: data.channel)
+
+	end
 end
